e07a53cb4e09f6310d1b6694bf731755ce6fde3a
Gianluca Brigandi <gbrigand@gmail.com>
2025-09-21 08:47:38 -0700

    Update Homebrew formula with SHA256 hashes for v0.1.2

--- HomebrewFormula/traverse.rb
+++ HomebrewFormula/traverse.rb
@@ -9,16 +9,16 @@ class Traverse < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/calltrace/traverse/releases/download/v#{version}/traverse-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "784282cc2ccfe07dc50671bbc1ea8ef57d83de02e49fcd23bd8e423f041cda7d"
     else
       url "https://github.com/calltrace/traverse/releases/download/v#{version}/traverse-macos-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_AMD64"
+      sha256 "6bde3109db57d70ec0ecf84133853a5ce22a5b3b88f8d4fc4e4f6315d06619ec"
     end
   end
 
   on_linux do
     url "https://github.com/calltrace/traverse/releases/download/v#{version}/traverse-linux-amd64.tar.gz"
-    sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+    sha256 "30cbe29a3bc418ea1d12a3ae7e6b6edbd8124d257b6cd05d3489ec7e2896c3cf"
   end
 
   def install
